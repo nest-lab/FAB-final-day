b7c5f70bf250867bfea81e9afcbf7eed3794ad61
Fabusuyi Ayodeji <fabgraphicsng@gmail.com>
2017-08-26 13:35:52 +0100

    Update ThirdFromLast.java

--- ThirdFromLast.java
+++ ThirdFromLast.java
@@ -12,6 +12,15 @@ package nestlab;
 public class ThirdFromLast {
     public static void  main(String[] args){
         int[] arr = {1,2,3,4,5,7,2,3};
-        System.out.println(arr[arr.length-3]);
+        int storeNum = 0;
+        //using Java predefined method
+        System.out.println(arr[arr.length-3]+"gotten using predefined method");
+        //implementing using Single Iteration
+        for(int i=0;i<arr.length;i++){
+            if(i>=2){
+            storeNum = arr[i-2];
+            }
+        }
+        System.out.println(storeNum+"gotten using single iteration");
     }
 }
